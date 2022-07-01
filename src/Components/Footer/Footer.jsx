fcf5ecd3d105a9d24e252f0e3e43d77fec8d9012
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-01 21:01:50 +0500

    Edit logos

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import useLocalization from "../../Hooks/useLocalization";
+
+import LogoEn from "../Lib/Icons/LogoEn";
+import LogoRu from "../Lib/Icons/LogoRu";
+
 const Footer = () => {
+  const [lang, setLang] = useLocalization(true);
+
   return (
     <footer className="footer">
-      <div className="container">footer</div>
+      <div className="container">
+        <Link className={"sdsd"} to={"/"}>
+          {lang === "ru" ? <LogoRu /> : <LogoEn />}
+        </Link>
+      </div>
     </footer>
   );
 };
