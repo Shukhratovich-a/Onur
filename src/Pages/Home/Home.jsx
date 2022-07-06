3b30a29c2833e41acd59f49f37bdffcbb5a9a4d2
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-06 20:47:16 +0500

    Edit home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,11 @@
 import Hero from "../../Components/Hero/Hero";
 
 const Home = () => {
-<<<<<<< HEAD
-  return <main className="main">Home </main>;
-=======
   return (
     <main className="main">
       <Hero />
     </main>
   );
->>>>>>> d1a149b6c7851e3f4766638de53fbc833c355dc3
 };
 
 export default Home;
