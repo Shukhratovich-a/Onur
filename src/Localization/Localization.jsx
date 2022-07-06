59e0f5a0a16b46703825ef76d124e5e1a7f06012
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-06 20:46:05 +0500

    Edit languages

--- src/Localization/Localization.jsx
+++ src/Localization/Localization.jsx
@@ -6,17 +6,16 @@ const Localization = {
       about: "about us",
       contacts: "contacts",
     },
-    
-    contact:{
-      title:"Let's talk",
-      text:"To request a quote or want to meet up for coffee contact us directly or fill out the form and we will  get back to you promptly.if you have any question you can call the our telephone",
-      title2:"Contact us",
-      inputTitle:"Fill the data",
-      input1:"Enter name",
-      input2:"Enter surname",
-      input3:"Enter number",
-      button:"Connect"
-      
+
+    contact: {
+      title: "Let's talk",
+      text: "To request a quote or want to meet up for coffee contact us directly or fill out the form and we will  get back to you promptly.if you have any question you can call the our telephone",
+      title2: "Contact us",
+      inputTitle: "Fill the data",
+      input1: "Enter name",
+      input2: "Enter surname",
+      input3: "Enter number",
+      button: "Connect",
     },
 
     hero: {
@@ -55,18 +54,18 @@ const Localization = {
     hero: {
       slider1: {
         heading: "Ehtiyojlarni sifatli mahsulotlar bilan qondirish",
-      },},
+      },
+    },
 
-    contact:{
-      title:"Давай поговорим",
-      text:"Чтобы запросить цитату или хотите встретиться за чашкой кофе, свяжитесь с нами напрямую или заполните форму, и мы свяжемся с вами в кратчайшие сроки. Если у вас есть какие-либо вопросы, вы можете позвонить по нашему телефону.",
-      title2:"Свяжись снами",
-      inputTitle:"Заполните данные",
-      input1:"Введите имя",
-      input2:"Введите фамилию",
-      input3:"Введите номер",
-      button:"Подключиться"
-
+    contact: {
+      title: "Давай поговорим",
+      text: "Чтобы запросить цитату или хотите встретиться за чашкой кофе, свяжитесь с нами напрямую или заполните форму, и мы свяжемся с вами в кратчайшие сроки. Если у вас есть какие-либо вопросы, вы можете позвонить по нашему телефону.",
+      title2: "Свяжись снами",
+      inputTitle: "Заполните данные",
+      input1: "Введите имя",
+      input2: "Введите фамилию",
+      input3: "Введите номер",
+      button: "Подключиться",
     },
 
     footer: {
@@ -100,18 +99,17 @@ const Localization = {
       slider1: {
         heading: "Ehtiyojlarni sifatli mahsulotlar bilan qondirish",
       },
-    
-    }б
-    contact:{
-      title:"Keling gaplashamiz",
-      text:"Narx so'rash yoki kofe ichishni xohlash uchun biz bilan bevosita bog'laning yoki shaklni to'ldiring va biz sizga tezda javob beramiz. Agar sizda biron bir savol bo'lsa, bizning telefonimizga qo'ng'iroq qilishingiz mumkin.",
-      title2:"Biz bilan bog'laning",
-      inputTitle:"Ma'lumotlarni to'ldiring",
-      input1:"Ismingizni kiriting",
-      input2:"Familiyangizni kiriting",
-      input3:"Raqamingizni kiriting",
-      button:"Ulanish"
+    },
 
+    contact: {
+      title: "Keling gaplashamiz",
+      text: "Narx so'rash yoki kofe ichishni xohlash uchun biz bilan bevosita bog'laning yoki shaklni to'ldiring va biz sizga tezda javob beramiz. Agar sizda biron bir savol bo'lsa, bizning telefonimizga qo'ng'iroq qilishingiz mumkin.",
+      title2: "Biz bilan bog'laning",
+      inputTitle: "Ma'lumotlarni to'ldiring",
+      input1: "Ismingizni kiriting",
+      input2: "Familiyangizni kiriting",
+      input3: "Raqamingizni kiriting",
+      button: "Ulanish",
     },
 
     footer: {
