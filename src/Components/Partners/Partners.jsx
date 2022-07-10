93c71723e3f03be0f1f79c40409bf3dc02384512
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-11 01:50:30 +0500

    Create partners section

---
+++ src/Components/Partners/Partners.jsx
@@ -0,0 +1,11 @@
+const Partners = () => {
+  return (
+    <section className="partners">
+      <div className={`container`}>
+        <h2 className={styles.partners__heading}>
+          
+        </h2>
+      </div>
+    </section>
+  );
+};
