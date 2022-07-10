a0964c1d7cac09ef1a95066e0c6de417d475f4a0
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-11 04:07:00 +0500

    Create product structure

--- src/Components/Partners/Partners.jsx
+++ src/Components/Partners/Partners.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import useLocaliztion from "../../Hooks/useLocalization";
 
 import styles from "./Partners.module.scss";
@@ -9,6 +11,25 @@ const Partners = () => {
     <section className={styles.partners}>
       <div className={`container`}>
         <h2 className={styles.partners__heading}>{localiztion.partners.heading}</h2>
+
+        <ul className={styles.partners__list}>
+          <li className={styles.partner}>
+            <div className={styles.partner__inner}>
+              <Link className={styles.partner__link} to={"/partners/nova"}>
+                Show products
+              </Link>
+
+              <a
+                className={styles.partner__link}
+                href="https://www.novaplastik.com/ru/"
+                rel="noreferrer noopener"
+                target={"_blank"}
+              >
+                Site
+              </a>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
