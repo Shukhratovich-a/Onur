6e88de2db6d3c9460b16915a01a7a5a69349c344
bekpulatovpro <jbekpulatov@yandex.ru>
2022-07-17 20:55:21 +0500

    added new files

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,5 +1,20 @@
+import {useParams} from "react-router-dom"
+
 const Products = () => {
-  return <main className="main"></main>;
+  const objects=[
+    {
+      id:1,
+      text:"bnma",
+      title:"title1"
+    }
+  ]
+  const {companyName}=useParams()
+  return (
+  <main className="main">
+    <div className="container">
+    {companyName}
+    </div>
+  </main>)
 };
 
 export default Products;
