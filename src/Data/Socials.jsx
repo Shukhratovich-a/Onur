a8b47085db5c56e0b89b18214909799cc8181945
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-21 14:35:23 +0500

    Create new data file

---
+++ src/Data/Socials.jsx
@@ -0,0 +1,10 @@
+const Socials = {
+  facebook: "https://facebook.com/onur_uz",
+  instagram: "https://instagram.com/Basmala2022",
+  telegram: "telegram",
+  tiktok: "tiktok",
+  phone: "phone",
+  mail: "onurgroup.uz@gmail.com",
+};
+
+export default Socials;
