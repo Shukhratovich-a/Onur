1f5a0fbb2f1bdb6b1f9fa9c2d16ba44c0f4dd0ca
Shukhratovich-a <shukhratovich138@gmail.com>
2022-07-21 14:40:42 +0500

    Edit products page

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,32 +1,28 @@
-import {useParams} from "react-router-dom"
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
 import Datas from "../../Data/Products";
-<<<<<<< HEAD
-=======
-import {useState} from "react"
-import styles from "./Product.module.scss"
-console.log(Datas);
->>>>>>> c9e20026383526fb1520bf57839f0e5770cee3d1
+
+import styles from "./Product.module.scss";
 
 const Products = () => {
-  const {companyName}=useParams();
-  const [products,setProducts]=useState(Datas[companyName])
-  
+  const { companyName } = useParams();
+  const [products, setProducts] = useState(Datas[companyName]);
+
   return (
-  <main className="main">
-    <div className="container">
-    <ul className={styles.product__list}>
-      {
-        products.map(((product)=>(
-          <li className={styles.product__item}>
-            <img className={styles.product__image} src={product.poster} alt={product.title} />
-            <h3 className={styles.product__title}>{product.title}</h3>
-            
-          </li>
-        )))
-      }
-    </ul>
-    </div>
-  </main>)
+    <main className="main">
+      <div className="container">
+        <ul className={styles.product__list}>
+          {products.map((product) => (
+            <li className={styles.product__item}>
+              <img className={styles.product__image} src={product.poster} alt={product.title} />
+              <h3 className={styles.product__title}>{product.title}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
 };
 
 export default Products;
