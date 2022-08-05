4fc2d28f8ea0e7aff54dcdff71c2310987d94d8f
Shukhratovich-a <shukhratovich138@gmail.com>
2022-08-05 09:28:27 +0500

    Add services component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../../Components/Hero/Hero";
+import Services from "../../Components/Services/Services";
 import Partners from "../../Components/Partners/Partners";
 
 const Home = () => {
   return (
     <main className="main">
       <Hero />
+      <Services />
       <Partners />
     </main>
   );
