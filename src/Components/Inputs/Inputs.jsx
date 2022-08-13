1c571fec20e317c0c87a9ee5cf135c859e8bc7b9
shukhratovich-a <shukhratovich138@gmail.com>
2022-08-13 21:18:15 +0500

    Connect to api

--- src/Components/Inputs/Inputs.jsx
+++ src/Components/Inputs/Inputs.jsx
@@ -10,41 +10,72 @@ import styles from "./Inputs.module.scss";
 
 function Inputs() {
   const localiztion = useLocalization();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const { firstname, lastname, contact } = evt.target.elements;
+
+    (async () => {
+      const responce = await fetch("http://localhost:5500/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: firstname.value.trim() + " " + lastname.value.trim(),
+          contact: contact.value.trim(),
+        }),
+      });
+
+      const data = await responce.json();
+
+      console.log(data);
+    })();
+  };
+
   return (
     <div className={styles.inputs}>
       <div className="container">
         <h3 className={styles.title}>{localiztion.contact.inputTitle}</h3>
+
         <div className={styles.input__flex}>
-          <div className={styles.inputs__flex__inner}>
-            <div className={styles.inputs__inner}>
+          <form className={styles.inputs__flex__inner} onSubmit={handleSubmit}>
+            <label className={styles.inputs__inner}>
               <input
                 className={styles.input}
                 type="text"
                 placeholder={localiztion.contact.input1}
+                name={"firstname"}
               />
 
               <Profile />
-            </div>
-            <div className={styles.inputs__inner}>
+            </label>
+
+            <label className={styles.inputs__inner}>
               <input
                 className={styles.input}
                 type="text"
                 placeholder={localiztion.contact.input2}
+                name={"lastname"}
               />
 
               <Person />
-            </div>
-            <div className={styles.inputs__inner}>
+            </label>
+
+            <label className={styles.inputs__inner}>
               <input
                 className={styles.input}
-                type="number"
+                type="tel"
                 placeholder={localiztion.contact.input3}
+                name={"contact"}
               />
 
               <Phone />
-            </div>
+            </label>
+
             <button className={styles.button}>{localiztion.contact.button}</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
