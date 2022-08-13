b10c914dc811128ed7c10c76b16c595a60b3384f
bekpulatovpro <jbekpulatov@yandex.ru>
2022-08-13 17:30:31 +0500

    add servise page

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -3,16 +3,30 @@ import React from "react";
 // import useLocalization from "../../Hooks/useLocalization";
 
 import ServicesList from "../../Components/Services/Services";
+import ServiceProducts from "../../Data/Products";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 import styles from "./Services.module.scss";
+import servicesProducts from "../../Data/ServiseProducts";
 
 const Services = () => {
-  
+  const { serviceName } = useParams();
+  const [serviceProducts, setServerProducts] = useState(ServiceProducts[serviceName]);
 
   return (
     <main>
       <section className={styles.services}>
-        <div className={`container ${styles.container}`}>'service'</div>
+        <div className={`container ${styles.container}`}>
+          <ul>
+            {
+              serviceProducts &&
+              <li>
+                <p>{servicesProducts.poster}</p>
+              </li>
+            }
+          </ul>
+        </div>
       </section>
       <ServicesList />
     </main>
