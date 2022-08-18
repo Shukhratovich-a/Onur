b08d3d1e9956a1bff6b3c62638756b40a851a3da
shukhratovich-a <shukhratovich138@gmail.com>
2022-08-19 00:47:45 +0500

    Edit any things

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper";
