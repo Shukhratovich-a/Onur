958f7dc019ca73f0c8d76ef06e3afd1a2ef7907b
shukhratovich-a <shukhratovich138@gmail.com>
2022-08-18 18:58:48 +0500

    Edit number

--- src/Components/Lib/Phone/Phone.jsx
+++ src/Components/Lib/Phone/Phone.jsx
@@ -5,8 +5,8 @@ import styles from "./Phone.module.scss";
 const Phone = () => {
   return (
     <div className={styles.phone}>
-      <a className={styles.phone__link} href="tel:+998951441333">
-        <span className={styles.phone__number}>+99895 144 13 33</span>
+      <a className={styles.phone__link} href="tel:+998555001222">
+        <span className={styles.phone__number}>+99855 500 12 22</span>
 
         <span className={styles.phone__icon}>
           <PhoneIcon />
