4267249dee6d3431b1f2f5ffc1856175c644f0a6
shukhratovich-a <shukhratovich138@gmail.com>
2022-08-19 01:57:07 +0500

    Add partners to home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../../Components/Hero/Hero";
 import Services from "../../Components/Services/Services";
+import Partners from "../../Components/Partners/Partners";
 
 const Home = () => {
   return (
     <main className="main">
       <Hero />
       <Services />
+      <Partners />
     </main>
   );
 };
