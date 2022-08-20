c8471e58716703c7d68984a49f096932061e3d9d
Shukhratovich-a <94617261+Shukhratovich-a@users.noreply.github.com>
2022-08-20 14:25:01 +0500

    Edit soervices data

--- src/Data/Services.jsx
+++ src/Data/Services.jsx
@@ -5,40 +5,114 @@ import Distribution from "../Assets/Images/ServicePage/Distribution.webp";
 import Accouting from "../Assets/Images/ServicePage/Accouting.webp";
 
 const servicesProducts = {
-  it: {
-    id: 1,
-    poster: It,
-    title: "It",
-    text: "XXI asrda hayotimizni IT texnalogiyalarsiz tasavvur qilib bo'lmaydi. Shuni hisobga olgan holda, bizning bu bo'lim alohida platforma bo'lib, asosiy vazifasi raqamlashtirishdan iborat. Android platformasi va Web platformasida ish olib borilmoqda. Yaqin kunlarda ushbu dasturlarimiz foydalanishga topshiriladi",
-  },
+  en: {
+    it: {
+      id: 1,
+      poster: It,
+      title: "It",
+      text: "XXI asrda hayotimizni IT texnalogiyalarsiz tasavvur qilib bo'lmaydi. Shuni hisobga olgan holda, bizning bu bo'lim alohida platforma bo'lib, asosiy vazifasi raqamlashtirishdan iborat. Android platformasi va Web platformasida ish olib borilmoqda. Yaqin kunlarda ushbu dasturlarimiz foydalanishga topshiriladi",
+    },
 
-  logistic: {
-    id: 2,
-    poster: Logistic,
-    title: "Logistic",
-    text: "2019 yilda Onur kompaniyasini qoshida Onur logistika bo'limi ochildi. Bu bo'lim Turkiyadan O'zbekistonga turli hildagi tovarlarni Fura(Tir) orqali olib keladi va yetkazib beradi. Ushbu jarayonda yuzaga keladigan dokument ishlarini ham bitkazib beradi.",
-  },
+    logistic: {
+      id: 2,
+      poster: Logistic,
+      title: "Logistic",
+      text: "2019 yilda Onur kompaniyasini qoshida Onur logistika bo'limi ochildi. Bu bo'lim Turkiyadan O'zbekistonga turli hildagi tovarlarni Fura(Tir) orqali olib keladi va yetkazib beradi. Ushbu jarayonda yuzaga keladigan dokument ishlarini ham bitkazib beradi.",
+    },
 
-  plumbing: {
-    id: 3,
-    poster: Plumbing,
-    title: "Plumbing",
-    text: "Turkiya davlatining en ko'zga ko'ringan kompaniyalari bilan hamkorlikda ishlaydigan bu bo'lim, 15 yildan ko'p vaqt mobaynida xalqimizning santexnikaga oid ehtiyojlarini qondirib kelmoqda.",
-  },
+    plumbing: {
+      id: 3,
+      poster: Plumbing,
+      title: "Plumbing",
+      text: "Turkiya davlatining en ko'zga ko'ringan kompaniyalari bilan hamkorlikda ishlaydigan bu bo'lim, 15 yildan ko'p vaqt mobaynida xalqimizning santexnikaga oid ehtiyojlarini qondirib kelmoqda.",
+    },
 
-  accounting: {
-    id: 4,
-    poster: Accouting,
-    title: "Accouting",
-    text: "Kichik va o'rta turdagi tadbirkorlarning asosiy muammolari bo'lgan xujjar ishlarini osonlashtirish maqsadida ushbu bo'limni ochishni maqsad qildik. Tez orada bu bo'limimiz ham o'z faoliyatini boshlaydi",
-  },
+    accounting: {
+      id: 4,
+      poster: Accouting,
+      title: "Accouting",
+      text: "Kichik va o'rta turdagi tadbirkorlarning asosiy muammolari bo'lgan xujjar ishlarini osonlashtirish maqsadida ushbu bo'limni ochishni maqsad qildik. Tez orada bu bo'limimiz ham o'z faoliyatini boshlaydi",
+    },
 
-  distribution: {
-    id: 5,
-    poster: Distribution,
-    title: "Distribution",
-    text: "Butun Toshken shahri va qisman Toshkent viloyatini O'z ichiga olgan ushbu bo'limda, 5000 dan ortiq qurilish soxasiga oid maxsulotlarni yetkazib beish bilan shug'ullanadi. Kelajakda bu rejalarni butun O'zbekiston bo'ylab amalga oshrish.",
+    distribution: {
+      id: 5,
+      poster: Distribution,
+      title: "Distribution",
+      text: "Butun Toshken shahri va qisman Toshkent viloyatini O'z ichiga olgan ushbu bo'limda, 5000 dan ortiq qurilish soxasiga oid maxsulotlarni yetkazib beish bilan shug'ullanadi. Kelajakda bu rejalarni butun O'zbekiston bo'ylab amalga oshrish.",
+    },
   },
+  ru: {
+    it: {
+      id: 1,
+      poster: It,
+      title: "It",
+      text: "XXI asrda hayotimizni IT texnalogiyalarsiz tasavvur qilib bo'lmaydi. Shuni hisobga olgan holda, bizning bu bo'lim alohida platforma bo'lib, asosiy vazifasi raqamlashtirishdan iborat. Android platformasi va Web platformasida ish olib borilmoqda. Yaqin kunlarda ushbu dasturlarimiz foydalanishga topshiriladi",
+    },
+
+    logistic: {
+      id: 2,
+      poster: Logistic,
+      title: "Logistic",
+      text: "2019 yilda Onur kompaniyasini qoshida Onur logistika bo'limi ochildi. Bu bo'lim Turkiyadan O'zbekistonga turli hildagi tovarlarni Fura(Tir) orqali olib keladi va yetkazib beradi. Ushbu jarayonda yuzaga keladigan dokument ishlarini ham bitkazib beradi.",
+    },
+
+    plumbing: {
+      id: 3,
+      poster: Plumbing,
+      title: "Plumbing",
+      text: "Turkiya davlatining en ko'zga ko'ringan kompaniyalari bilan hamkorlikda ishlaydigan bu bo'lim, 15 yildan ko'p vaqt mobaynida xalqimizning santexnikaga oid ehtiyojlarini qondirib kelmoqda.",
+    },
+
+    accounting: {
+      id: 4,
+      poster: Accouting,
+      title: "Accouting",
+      text: "Kichik va o'rta turdagi tadbirkorlarning asosiy muammolari bo'lgan xujjar ishlarini osonlashtirish maqsadida ushbu bo'limni ochishni maqsad qildik. Tez orada bu bo'limimiz ham o'z faoliyatini boshlaydi",
+    },
+
+    distribution: {
+      id: 5,
+      poster: Distribution,
+      title: "Distribution",
+      text: "Butun Toshken shahri va qisman Toshkent viloyatini O'z ichiga olgan ushbu bo'limda, 5000 dan ortiq qurilish soxasiga oid maxsulotlarni yetkazib beish bilan shug'ullanadi. Kelajakda bu rejalarni butun O'zbekiston bo'ylab amalga oshrish.",
+    },
+  },
+  uz: {
+    it: {
+      id: 1,
+      poster: It,
+      title: "It",
+      text: "XXI asrda hayotimizni IT texnalogiyalarsiz tasavvur qilib bo'lmaydi. Shuni hisobga olgan holda, bizning bu bo'lim alohida platforma bo'lib, asosiy vazifasi raqamlashtirishdan iborat. Android platformasi va Web platformasida ish olib borilmoqda. Yaqin kunlarda ushbu dasturlarimiz foydalanishga topshiriladi",
+    },
+
+    logistic: {
+      id: 2,
+      poster: Logistic,
+      title: "Logistic",
+      text: "2019 yilda Onur kompaniyasini qoshida Onur logistika bo'limi ochildi. Bu bo'lim Turkiyadan O'zbekistonga turli hildagi tovarlarni Fura(Tir) orqali olib keladi va yetkazib beradi. Ushbu jarayonda yuzaga keladigan dokument ishlarini ham bitkazib beradi.",
+    },
+
+    plumbing: {
+      id: 3,
+      poster: Plumbing,
+      title: "Plumbing",
+      text: "Turkiya davlatining en ko'zga ko'ringan kompaniyalari bilan hamkorlikda ishlaydigan bu bo'lim, 15 yildan ko'p vaqt mobaynida xalqimizning santexnikaga oid ehtiyojlarini qondirib kelmoqda.",
+    },
+
+    accounting: {
+      id: 4,
+      poster: Accouting,
+      title: "Accouting",
+      text: "Kichik va o'rta turdagi tadbirkorlarning asosiy muammolari bo'lgan xujjar ishlarini osonlashtirish maqsadida ushbu bo'limni ochishni maqsad qildik. Tez orada bu bo'limimiz ham o'z faoliyatini boshlaydi",
+    },
+
+    distribution: {
+      id: 5,
+      poster: Distribution,
+      title: "Distribution",
+      text: "Butun Toshken shahri va qisman Toshkent viloyatini O'z ichiga olgan ushbu bo'limda, 5000 dan ortiq qurilish soxasiga oid maxsulotlarni yetkazib beish bilan shug'ullanadi. Kelajakda bu rejalarni butun O'zbekiston bo'ylab amalga oshrish.",
+    },
+  }
 };
 
 export default servicesProducts;
