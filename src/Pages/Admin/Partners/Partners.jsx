924184bf2c99370eeffdb56d921b301f146b0046
bekpulatovpro <jbekpulatov@yandex.ru>
2022-08-22 18:19:59 +0500

    added users page

--- src/Pages/Admin/Partners/Partners.jsx
+++ src/Pages/Admin/Partners/Partners.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import AdminPartners from "../../../Components/AdminPartners/AdminPartners";
 import PartnerEdit from "../PartnerEdit/PartnerEdit";
+import Users from "../Users/Users"
 
 const Partners = () => {
   return (
     <main className="main">
       <Routes>
         <Route path={"/"} element={<AdminPartners />} />
+        <Route path={"/users"} element={<Users />} />
         <Route path={"/:partnerId"} element={<AdminPartners />} />
         <Route path={"/create"} element={<PartnerEdit />} />
         <Route path={"/edit/:partnerId"} element={<PartnerEdit />} />
