00c6bc432c1f35290ba01e637c55dd843b044ae8
shukhratovich-a <shukhratovich138@gmail.com>
2022-08-23 04:36:52 +0500

    Finish partners and products

--- src/Pages/Admin/ProductEdit/ProductEdit.jsx
+++ src/Pages/Admin/ProductEdit/ProductEdit.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Modal, Box } from "@mui/material";
+import { Add } from "@mui/icons-material";
 
 import useToken from "../../../Hooks/useToken";
 
@@ -10,14 +11,24 @@ import Images from "../../../Components/Lib/Icons/Images";
 
 import styles from "./ProductEdit.module.scss";
 
-const PartnerEdit = () => {
+const ProductEdit = () => {
   const navigate = useNavigate();
   let { productId, partnerId } = useParams();
 
   const [token] = useToken();
+  const [isOpen, setIsOpen] = React.useState(false);
 
   const [name, setName] = React.useState("");
   const [image, setImage] = React.useState("");
+
+  const [params, setParams] = React.useState([]);
+
+  const paramObj = {
+    productParamName: "",
+    productParamText: "",
+    productParamId: params.length ? params.at(-1).productParamId + 1 : 1,
+    productId,
+  };
 
   React.useEffect(() => {
     if (productId) {
@@ -29,6 +40,7 @@ const PartnerEdit = () => {
         if (data.status === 200) {
           setImage(() => data.data.productImage);
           setName(() => data.data.productName);
+          setParams(() => data.data.productParams);
         } else {
           navigate(-1);
         }
@@ -83,20 +95,75 @@ const PartnerEdit = () => {
     })();
   };
 
+  const saveParams = () => {
+    const result = {
+      productId,
+    };
+
+    result.params = params.map((param) => {
+      if (param.productParamName.length > 0 && param.productParamText.length > 0) {
+        return {
+          productParamName: param.productParamName,
+          productParamText: param.productParamText,
+        };
+      } else {
+        return null;
+      }
+    });
+
+    (async () => {
+      const responce = await fetch(HOST + "/productparams", {
+        method: "POST",
+        headers: {
+          token: token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(result),
+      });
+
+      const data = await responce.json();
+
+      if (data.status === 201) {
+        setIsOpen(true);
+      }
+    })();
+  };
+
   return (
     <main className="main">
-      <section className={`${styles.partner}`}>
+      <section className={`${styles.product}`}>
+        <Modal
+          className={`${styles.product__modal}`}
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          aria-labelledby="parent-modal-title"
+          aria-describedby="parent-modal-description"
+        >
+          <Box className={`${styles.product__modal__inner}`}>
+            <span>Saved</span>
+
+            <Button
+              className={`${styles.product__modal__button}`}
+              variant={"contained"}
+              type={"submit"}
+              onClick={() => setIsOpen(false)}
+            >
+              ok
+            </Button>
+          </Box>
+        </Modal>
+
         <div className={`${styles.container} container`}>
-          <form className={`${styles.partner__form}`} onSubmit={handleSubmit}>
-            <label className={`${styles.partner__form__label}`}>
+          <form className={`${styles.product__form}`} onSubmit={handleSubmit}>
+            <label className={`${styles.product__form__label}`}>
               <div
-                className={`${styles.partner__form__inner} ${
-                  image ? styles["partner__form__inner--image"] : ""
+                className={`${styles.product__form__inner} ${
+                  image ? styles["product__form__inner--image"] : ""
                 }`}
               >
                 {image ? (
                   <img
-                    className={`${styles.partner__form__image}`}
+                    className={`${styles.product__form__image}`}
                     src={image}
                     alt="Select file"
                     width={500}
@@ -104,7 +171,7 @@ const PartnerEdit = () => {
                   />
                 ) : null}
 
-                <div className={`${styles.partner__form__wrapper}`}>
+                <div className={`${styles.product__form__wrapper}`}>
                   <Images />
                   <span>Upload profile image here</span>
                   <span>(JPG, PNG)</span>
@@ -112,7 +179,7 @@ const PartnerEdit = () => {
               </div>
 
               <input
-                className={`${styles.partner__form__input} visually-hidden`}
+                className={`${styles.product__form__input} visually-hidden`}
                 name="file"
                 type="file"
                 onChange={(evt) => {
@@ -123,9 +190,9 @@ const PartnerEdit = () => {
               />
             </label>
 
-            <div className={`${styles.partner__form__aside}`}>
+            <div className={`${styles.product__form__aside}`}>
               <TextField
-                className={`${styles.partner__form__input}`}
+                className={`${styles.product__form__input}`}
                 name={"name"}
                 label={"Name"}
                 value={name}
@@ -135,7 +202,7 @@ const PartnerEdit = () => {
               />
 
               <Button
-                className={`${styles.partner__form__button}`}
+                className={`${styles.product__form__button}`}
                 variant={"contained"}
                 type={"submit"}
                 fullWidth
@@ -145,7 +212,7 @@ const PartnerEdit = () => {
 
               {productId ? (
                 <Button
-                  className={`${styles.partner__form__button}`}
+                  className={`${styles.product__form__button}`}
                   variant={"contained"}
                   type={"button"}
                   fullWidth
@@ -156,10 +223,85 @@ const PartnerEdit = () => {
               ) : null}
             </div>
           </form>
+
+          <ul className={`${styles.product__params}`}>
+            {params.length > 0 &&
+              params.map((param) => (
+                <li className={`${styles.product__param}`} key={param.productParamId}>
+                  <TextField
+                    className={`${styles.product__param__input}`}
+                    name={"name"}
+                    label={"Name"}
+                    defaultValue={param.productParamName}
+                    variant={"outlined"}
+                    onChange={(evt) => {
+                      const array = [...params];
+                      const data = array.find(
+                        (item) => item.productParamId === param.productParamId
+                      );
+                      data.productParamName = evt.target.value;
+                      setParams(array);
+                    }}
+                  />
+
+                  <TextField
+                    className={`${styles.product__param__input}`}
+                    name={"text"}
+                    label={"Text"}
+                    defaultValue={param.productParamText}
+                    variant={"outlined"}
+                    onChange={(evt) => {
+                      const array = [...params];
+                      const data = array.find(
+                        (item) => item.productParamId === param.productParamId
+                      );
+                      data.productParamText = evt.target.value;
+                      setParams(array);
+                    }}
+                  />
+                </li>
+              ))}
+          </ul>
+
+          {productId && (
+            <div className={`${styles.product__bottom}`}>
+              {params.length < 10 && (
+                <Button
+                  className={`${styles.products__bottom__button}`}
+                  variant={"contained"}
+                  type={"button"}
+                  onClick={() => {
+                    const array = [...params];
+                    if (params.length === 0) {
+                      array.push(paramObj);
+                      setParams(array);
+                    } else {
+                      if (params.length >= 10) return;
+                      if (params.at(-1).productParamName === "") return;
+                      if (params.at(-1).productParamText === "") return;
+                      array.push(paramObj);
+                      setParams(array);
+                    }
+                  }}
+                >
+                  <Add />
+                </Button>
+              )}
+
+              <Button
+                className={`${styles.products__bottom__button}`}
+                variant={"contained"}
+                type={"button"}
+                onClick={saveParams}
+              >
+                Save Params
+              </Button>
+            </div>
+          )}
         </div>
       </section>
     </main>
   );
 };
 
-export default PartnerEdit;
+export default ProductEdit;
